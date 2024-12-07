9df71dd6ad21177c20e3102e549d8eac7f9b82db
ahmadeldiasty98 <ahmadeldiasty98@gmail.com>
2024-12-07 20:43:34 +0200

    crud operations for hotel

--- Hotel_Track/Controllers/HotelController.cs
+++ Hotel_Track/Controllers/HotelController.cs
@@ -19,7 +19,119 @@ namespace Hotel_Track.Controllers
             this.roomRepository = roomRepository;
             this.hotelRepository = hotelRepository;
         }
-        public IActionResult Index(int roomId)
+        //List of hotels
+        public IActionResult Index()
+        {
+            var hotels = hotelRepository.GetAll().ToList();
+            return View(hotels); 
+        }
+        //Add New Hotel
+        public IActionResult Create()
+        {
+            Hotel hotel = new Hotel();
+            ViewBag.HotelType = Enum.GetValues(typeof(EnumHotelType))
+         .Cast<EnumHotelType>()
+         .Select(e => new { Id = (int)e, Name = e.ToString() })
+         .ToList();
+            return View(hotel);
+        }
+        [HttpPost]
+        public IActionResult Create(Hotel hotel, IFormFile ImgUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ImgUrl.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ImgUrl.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "Hotels", fileName);
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        ImgUrl.CopyTo(stream);
+                    }
+
+                    hotel.ImgUrl = fileName;
+                }
+
+                hotelRepository.Add(hotel);
+                hotelRepository.Commit();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(hotel);
+        }
+
+        public IActionResult Edit(int HotelId)
+        {
+            var hotel = hotelRepository.GetOne(null, e => e.Id == HotelId);
+            ViewBag.HotelType = Enum.GetValues(typeof(EnumHotelType))
+         .Cast<EnumHotelType>()
+         .Select(e => new { Id = (int)e, Name = e.ToString() })
+         .ToList();
+            return View(hotel);
+        }
+        [HttpPost]
+        public IActionResult Edit(Hotel hotel, IFormFile ImgUrl)
+        {
+            var oldHotel = hotelRepository.GetOne(null,e => e.Id == hotel.Id);
+            if (ModelState.IsValid)
+            {
+                if (ImgUrl != null && ImgUrl.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ImgUrl.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "Hotels", fileName);
+                    var oldFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "Hotels", oldHotel.ImgUrl);
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        ImgUrl.CopyTo(stream);
+                    }
+
+                    if (System.IO.File.Exists(oldFilePath))
+                    {
+                        System.IO.File.Delete(oldFilePath);
+                    }
+
+                    hotel.ImgUrl = fileName;
+                }
+                else
+                {
+                    hotel.ImgUrl = oldHotel.ImgUrl;
+                }
+
+               hotelRepository.Edit(hotel);
+                hotelRepository.Commit();
+
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.HotelType = Enum.GetValues(typeof(EnumHotelType))
+         .Cast<EnumHotelType>()
+         .Select(e => new { Id = (int)e, Name = e.ToString() })
+         .ToList();
+            hotel.ImgUrl = oldHotel.ImgUrl;
+            return View(hotel);
+
+        }
+
+        public IActionResult Delete(int hotelId)
+        {
+            var hotel = hotelRepository.GetOne(null, e => e.Id == hotelId);
+
+            var oldFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "Hotels",hotel.ImgUrl);
+
+            if (System.IO.File.Exists(oldFilePath))
+            {
+                System.IO.File.Delete(oldFilePath);
+            }
+
+            hotelRepository.Delete(hotel);
+            hotelRepository.Commit();
+
+            return RedirectToAction(nameof(Index));
+        }
+        //ٌget one room of an hotel rooms
+        public IActionResult Room(int roomId)
         {
             var room = roomRepository.GetOne(null, e => e.Id == roomId);
             var hotelId = room.HotelId;
@@ -27,6 +139,7 @@ namespace Hotel_Track.Controllers
             ViewData["HotelName"] = hotel.Name;
             return View(room);
         }
+        //get all rooms in one hotel
         public IActionResult Rooms(int hotelId)
         {
             var rooms = roomRepository.GetAll(inculdeProp: null,expression: r => r.HotelId == hotelId,tracked: false).ToList();
